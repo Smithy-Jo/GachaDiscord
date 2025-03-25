28923e32ea6b09b4122de8182110db444259452d
Smithy-Jo <baptiste.clayette@gmail.com>
2025-03-25 04:55:29 +0100

    Modification de l'envoie des invocations     Evoie du recapitulatif des invocations dans le salon     Evoie d'un message privé pour chaque invocation

--- commands/gacha/invoc.js
+++ commands/gacha/invoc.js
@@ -28,62 +28,63 @@ module.exports = {
                     .setStyle(ButtonStyle.Primary),
             );
 
-        try {
-            // Envoi du message privé avec les boutons
-            const dmChannel = await interaction.user.createDM();
-            const message = await dmChannel.send({
-                content: `Vous avez ${userModelInstance.balance} pièces. Combien voulez-vous invoquer ?`,
-                components: [row],
-            });
+        const message = await interaction.reply({
+            content: `Vous avez ${userModelInstance.balance} pièces. Combien voulez-vous invoquer ?`,
+            components: [row],
+        });
+        const collector = message.createMessageComponentCollector({
+            filter: i => i.user.id === interaction.user.id,
+            time: 20_000, // 20 secondes pour répondre
+        });
 
-            await interaction.reply(`${interaction.user.username}, je vous ai envoyé un message privé !`);
+        collector.on('collect', async i => {
 
-            // Création du collector pour écouter les interactions des boutons
-            const collector = message.createMessageComponentCollector({
-                filter: i => i.user.id === interaction.user.id,
-                time: 60000, // 1 minute pour répondre
-            });
+            if ((i.customId === 'btn_invoc_1' && userModelInstance.balance < 100) || (i.customId === 'btn_invoc_10' && userModelInstance.balance < 1000)) {
+                await i.update({ content: 'Vous n\'avez pas assez de pièces pour invoquer !', components: [] });
 
-            collector.on('collect', async i => {
+            } else if (i.customId === 'btn_invoc_1') {
+                const character = await Character.invoc(userModelInstance);
+                userModelInstance.balance -= 100;
+                userModelInstance.updatePitySystem(character.rarity);
+                await userModelInstance.save();
+                await i.update({ content: '', components: [], embeds: [character.generateEmbed()] });
 
-                if ((i.customId === 'btn_invoc_1' && userModelInstance.balance < 100) || (i.customId === 'btn_invoc_10' && userModelInstance.balance < 1000)) {
-                    await i.update({ content: 'Vous n\'avez pas assez de pièces pour invoquer !', components: [] });
-
-                } else if (i.customId === 'btn_invoc_1') {
+            } else if (i.customId === 'btn_invoc_10') {
+                // Invocation de 10 personnages
+                let characters = [];
+                for (let i = 0; i < 10; i++) {
                     const character = await Character.invoc(userModelInstance);
-                    userModelInstance.balance -= 100;
+                    characters.push(character);
                     userModelInstance.updatePitySystem(character.rarity);
-                    await userModelInstance.save();
-                    await i.update({ content: '', components: [], embeds: [character.generateEmbed()] });
-
-                } else if (i.customId === 'btn_invoc_10') {
-                    let characters = [];
-                    for (let i = 0; i < 10; i++) {
-                        const character = await Character.invoc(userModelInstance);
-                        characters.push(character);
-                        userModelInstance.updatePitySystem(character.rarity);
-                    }
-                    userModelInstance.balance -= 1000;
-                    await userModelInstance.save();
-                    await i.update({ content: '', components: [], embeds: characters.map(character => character.generateEmbed()) });
+                }
+                userModelInstance.balance -= 1000;
+                await userModelInstance.save();
+                // Envoie des 10 personnages dans le chanel de l'interaction
+                await i.update({
+                    content: `${i.user.username} a invoqué 10 personnages !\n\n` +
+                        characters.map(character => `${character.name} ${character.formatRarity()}`).join('\n'),
+                    components: []
+                });
+                // Envoie des details des 10 personnages dans un message privé à l'utilisateur
+                try {
+                    await interaction.user.send({
+                        content: `Vous avez invoqué 10 personnages !`,
+                        embeds: characters.map(character => character.generateEmbed())
+                    });
+                }
+                catch (error) {
+                    await interaction.followUp('Impossible de vous envoyer les détails des personnages en message privé.');
                 }
 
-                collector.stop();
-            });
+            }
 
-            collector.on('end', async collected => {
-                if (collected.size === 0) {
-                    await message.edit({ content: "⏳ Temps écoulé, vous n'avez pas fait de choix.", components: [] });
-                }
-            });
+            collector.stop();
+        });
 
-
-        } catch (error) {
-            console.error(error);
-            if (interaction.replied)
-                return interaction.editReply(`${interaction.user.username}, je n'ai pas pu vous envoyer de message privé.\nVeuillez vérifier vos paramètres de confidentialité.\nContatez un administrateur si le problème persiste.`);
-            else
-                return interaction.reply(`${interaction.user.username}, je n'ai pas pu vous envoyer de message privé.\nVeuillez vérifier vos paramètres de confidentialité.\nContatez un administrateur si le problème persiste.`);
-        }
+        collector.on('end', async collected => {
+            if (collected.size === 0) {
+                await message.edit({ content: "⏳ Temps écoulé, vous n'avez pas fait de choix.", components: [] });
+            }
+        });
     }
 }
